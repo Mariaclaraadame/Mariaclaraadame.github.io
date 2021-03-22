d7b4b1ca267815b3c3a3f70275d33e0925eba6ce
Mariaclaraadame <mariaclaraadame@hotmail.com>
2021-03-21 22:09:00 -0300

    catalogo

--- js/desafios2/3.js
+++ js/desafios2/3.js
@@ -3,7 +3,7 @@ $(function () {
   $.get(link, function (data) {
     console.log(data);
     $.each(data, function (index, filme) {
-      let $divCatalogo = $("<div>").addClass("catalogo");
+      let $catalogo = $("<div>").addClass("catalogo");
       let $figura = $("<img>").attr("src", filme.figura).addClass("figura");
       let $titulo = $("<h1>").text(filme.titulo).addClass("titulo");
       let $genero = $("<p>").text(filme.generos.join(", ")).addClass("generos");
@@ -17,59 +17,59 @@ $(function () {
         case 4:
           {
             $estrela = $("<img>")
-              .attr("src", "4-estrelas.png")
+              .attr("src", "../../imagens/4-estrelas.png")
               .addClass("estrela");
           }
           break;
         case 5: {
           $estrela = $("<img>")
-            .attr("src", "../../imagens/5-estrelas")
+            .attr("src", "../../imagens/5-estrelas.png")
             .addClass("estrela");
         }
       }
-      $divCatalogo.append($figura);
-      $divCatalogo.append($titulo);
+      $catalogo.append($figura);
+      $catalogo.append($titulo);
       let classificacao = filme.classificacao;
       if (classificacao == 18) {
         $classificaoetaria = $("<img>")
-          .attr("src", "18.png")
+          .attr("src", "../../imagens/18.png")
           .addClass("classificacao");
-        $divCatalogo.append($classificaoetaria);
+        $catalogo.append($classificaoetaria);
       } else if (classificacao == 16) {
         $classificaoetaria = $("<img>")
-          .attr("src", "16.png")
+          .attr("src", "../../imagens/16.png")
           .addClass("classificacao");
-        $divCatalogo.append($classificaoetaria);
+        $catalogo.append($classificaoetaria);
       } else if (classificacao == 14) {
         $classificaoetaria = $("<img>")
-          .attr("src", "14.png")
+          .attr("src", "../../imagens/14.png")
           .addClass("classificacao");
-        $divCatalogo.append($classificaoetaria);
+        $catalogo.append($classificaoetaria);
       } else if (classificacao == 12) {
         $classificaoetaria = $("<img>")
-          .attr("src", "12.png")
+          .attr("src", "../../imagens/12.png")
           .addClass("classificacao");
-        $divCatalogo.append($classificaoetaria);
+        $catalogo.append($classificaoetaria);
       } else {
         $classificaoetaria = $("<img>")
-          .attr("src", "livre.png")
+          .attr("src", "../../imagens/livre.png")
           .addClass("classificacao");
-        $divCatalogo.append($classificaoetaria);
+        $catalogo.append($classificaoetaria);
       }
-      $divCatalogo.append($estrela);
-      $divCatalogo.append($genero);
-      $divCatalogo.append($sinopse);
-      $divCatalogo.append($elenco);
-      $divCatalogo.append($semelhantes);
+      $catalogo.append($estrela);
+      $catalogo.append($genero);
+      $catalogo.append($sinopse);
+      $catalogo.append($elenco);
+      $catalogo.append($semelhantes);
       $.each(filme.titulosSemelhantes, function (idx, titulo) {
         let $tituloSemelhante = $("<li>").text(
           "| " + data[titulo - 1].titulo + " | "
         );
         $tituloSemelhante.addClass("semelhantes");
-        $divCatalogo.append($tituloSemelhante);
+        $catalogo.append($tituloSemelhante);
       });
 
-      $("body").append($divCatalogo);
+      $("body").append($catalogo);
     });
   });
 });
