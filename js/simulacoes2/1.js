bc16fb3f80fe410ea8157539db34db4b96ce8e12
Mariaclaraadame <mariaclaraadame@hotmail.com>
2021-05-07 23:43:42 -0300

    soluções

--- js/simulacoes2/1.js
+++ js/simulacoes2/1.js
@@ -1,60 +1,30 @@
 $(function () {
-  $("#botao").click(function () {
-    let soluto = $("#soluto").val();
-    let solvente = $("#solvente").val();
-    suluto = parseFloat(soluto);
-    solvente = parseFloat(solvente);
-    let multiplicacao = 800 * solvente;
-    if (multiplicacao > soluto) {
-      $("#resultado").html(
-        "1L água - 800g nitrato de Bário<br>" +
-          solvente +
-          "L água - Xg açúcar<br> 1 . X = 800 . " +
-          solvente +
-          "<br>X = " +
-          mult1 +
-          "g<br> Era esperado " +
-          mult1 +
-          "g<br>Você forneceu " +
-          soluto +
-          "g,  então <b>solução insaturada.</b>"
+  $("#botao").click(function (event) {
+    event.preventDefault();
+    const soluto = parseFloat($("#soluto").val());
+    const solvente = parseFloat($("#solvente").val());
+    const multiplicacao = 800 * solvente;
+    if (multiplicacao === soluto) {
+      $(".resultado").html(
+        `<p>1L água - 800g nitrato de Bário</p> 
+         <p>${solvente}L água - X nitrato de Bário</p> 
+         <p>X = ${multiplicacao}</p> 
+         <p class="resp small">Era esperado "${multiplicacao}"g nitrato de Bário </br> Você forneceu "${soluto}"g nitrato de Bário </br> Então, essa solução é saturada.</p>`
+      );
+    } else if (multiplicacao > soluto) {
+      $(".resultado").html(
+        `<p>1L água - 800g nitrato de Bário</p> 
+         <p>${solvente}L água - X nitrato de Bário</p> 
+         <p>X = ${multiplicacao}</p> 
+         <p class="resp small">Era esperado "${multiplicacao}"g nitrato de Bário </br> Você forneceu "${soluto}"g nitrato de Bário </br> Então, essa solução é insaturada.</p>`
+      );
+    } else {
+      $(".resultado").html(
+        `<p>1L água - 800g nitrato de Bário</p> 
+         <p>${solvente}L água - X nitrato de Bário</p> 
+         <p>X = ${multiplicacao}</p> 
+         <p class="resp small">Era esperado "${multiplicacao}"g nitrato de Bário </br> Você forneceu "${soluto}"g nitrato de Bário </br> Então, essa solução é supersaturada.</p>`
       );
     }
-    if (mult1 < soluto) {
-      $("#resultado").html(
-        "1L água - 330g açúcar<br>" +
-          solvente +
-          "L água - Xg açúcar<br> 1 . X = 330 . " +
-          solvente +
-          "<br>X = " +
-          mult1 +
-          "g<br> Era esperado " +
-          mult1 +
-          "g<br>Você forneceu " +
-          soluto +
-          "g,  então <b>solução supersaturada.</b>"
-      );
-    }
-    if (mult1 == soluto) {
-      $("#resultado").html(
-        "1L água - 330g açúcar<br>" +
-          solvente +
-          "L água - Xg açúcar<br> 1 . X = 330 . " +
-          solvente +
-          "<br>X = " +
-          mult1 +
-          "g<br> Era esperado " +
-          mult1 +
-          "g<br>Você forneceu " +
-          soluto +
-          "g,  então <b>solução saturada.</b>"
-      );
-    }
-  });
-
-  $("#limpar").click(function () {
-    $("#solvente").val("");
-    $("#soluto").val("");
-    $("#resultado").text("");
   });
 });
---
+++ js/simulacoes2/2.js
@@ -0,0 +1,22 @@
+$(function () {
+  $("#botao").click(function (event) {
+    event.preventDefault();
+    const soluto = parseFloat($("#soluto").val());
+    const solvente = parseFloat($("#solvente").val());
+    const massaMolarNB = 261;
+    const molaridade = (soluto / (massaMolarNB * solvente)).toFixed(5);
+    const solubilidade = 100 * (soluto / (solvente * 1000));
+    const texto = `<p><b>1° passo: </b> Molaridade = Massa do soluto / Massa molar do soluto x Volume do solvente</p> 
+                   <p><b>2° passo: </b> Molaridade = ${soluto} / (${massaMolarNB} x ${solvente})</p>  
+                   <p class="resp small">A concentração molar (molaridade) da solução é ${molaridade}</p>`;
+    if (soluto > solubilidade) {
+      $(".resultado").html(
+        `${texto} <p class="resp small">Havendo formação de corpo de fundo.</p>`
+      );
+    } else {
+      $(".resultado").html(
+        `${texto} <p class="resp small">Não havendo formação de corpo de fundo.</p>`
+      );
+    }
+  });
+});
